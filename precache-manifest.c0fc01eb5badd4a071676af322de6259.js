1555ebdc1cb62e066d2578b03a3690838a43608b
andreearig <andreearigmanyi8@gmail.com>
2020-08-17 16:20:24 +0300

    Updates

---
+++ precache-manifest.c0fc01eb5badd4a071676af322de6259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44209eebae8696b129fd922d5b983edc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1d1ceac142217bab1b34",
+    "url": "/monsters-rolodex/static/css/main.df26bd98.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1ceac142217bab1b34",
+    "url": "/monsters-rolodex/static/js/main.a9078c5e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
